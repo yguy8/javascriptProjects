216919e7f354e06a124056238a9be6edce9a772a
Clara Jenhee Franco Aguilar <clara20franco@gmail.com>
2025-12-18 18:32:16 -0600

    cambio de brújula

--- islaDelColor/inicio.js
+++ islaDelColor/inicio.js
@@ -1,5 +1,10 @@
+//función del boton para ir a la sección explorar
 function irASeccion() {
     document.getElementById("explorar").scrollIntoView({ behavior: "smooth" });
 }
 
+//botón de la brújula para mostrar información
+const btnBrujula = document.getElementById("btnBrujula"); const modalBrujula = document.getElementById("modalBrujula"); const cerrarModal = document.getElementById("cerrarModal"); btnBrujula.addEventListener("click", () => { modalBrujula.style.display = "block"; }); cerrarModal.addEventListener("click", () => { modalBrujula.style.display = "none"; });  window.addEventListener("click", (e) => { if (e.target === modalBrujula) { modalBrujula.style.display = "none"; } });
+
+//funciones para abrir y cerrar tesoros (modales)
 function openModal(id) { document.getElementById(id).style.display = "block"; } function closeModal(id) { document.getElementById(id).style.display = "none"; }
