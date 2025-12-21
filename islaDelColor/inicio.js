66fe300f3b4b8f7fa6d8d4d572f4636b0de082aa
Clara Jenhee Franco Aguilar <clara20franco@gmail.com>
2025-12-20 18:08:45 -0600

    guia de ayuda agregada

--- islaDelColor/inicio.js
+++ islaDelColor/inicio.js
@@ -3,8 +3,8 @@ function irASeccion() {
     document.getElementById("explorar").scrollIntoView({ behavior: "smooth" });
 }
 
-//botón de la brújula para mostrar información
-const btnBrujula = document.getElementById("btnBrujula"); const modalBrujula = document.getElementById("modalBrujula"); const cerrarModal = document.getElementById("cerrarModal"); btnBrujula.addEventListener("click", () => { modalBrujula.style.display = "block"; }); cerrarModal.addEventListener("click", () => { modalBrujula.style.display = "none"; });  window.addEventListener("click", (e) => { if (e.target === modalBrujula) { modalBrujula.style.display = "none"; } });
+//botón de la guía para mostrar información
+const btnGuia = document.getElementById("btnGuia"); const modalGuia = document.getElementById("modalGuia"); const cerrarModal = document.getElementById("cerrarModal"); btnGuia.addEventListener("click", () => { modalGuia.style.display = "block"; }); cerrarModal.addEventListener("click", () => { modalGuia.style.display = "none"; });  window.addEventListener("click", (e) => { if (e.target === modalGuia) { modalGuia.style.display = "none"; } });
 
 //funciones para abrir y cerrar tesoros (modales)
 function openModal(id) { document.getElementById(id).style.display = "block"; } function closeModal(id) { document.getElementById(id).style.display = "none"; }
